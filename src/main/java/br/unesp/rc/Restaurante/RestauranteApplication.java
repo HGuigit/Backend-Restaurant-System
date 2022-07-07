04a5589f79a9ef50129dc3ba102684347eea75ed
hguigit <guilhermesps2@gmail.com>
2022-07-07 11:09:56 -0300

    First commit, setting up spring boot with spring initializr

---
+++ src/main/java/br/unesp/rc/Restaurante/RestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.Restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteApplication.class, args);
+	}
+
+}
